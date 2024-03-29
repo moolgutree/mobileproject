d81c219c58ed8fd55d6891911e7a27dfaefde0a2
Lee Eunjun <85077262+moolgutree@users.noreply.github.com>
2024-03-30 01:35:27 +0900

    Update MainActivity.kt

--- 과제/MainActivity.kt
+++ 과제/MainActivity.kt
@@ -1,14 +1,30 @@
-package kr.ac.dmu.ai032802
+package kr.ac.dmu.ai0328
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kr.ac.dmu.ai032802.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import kr.ac.dmu.ai0328.databinding.ActivityMainBinding
+import java.lang.Integer.parseInt
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        var vb = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(vb.root)
+        //setContentView(R.layout.activity_main)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var num1: String
+        var num2: String
+        var result: Int
+
+        binding.btnPlus.setOnClickListener {
+
+
+            num1 = binding.editNum1.getText().toString();
+            num2 = binding.editNum2.getText().toString();
+            result = Integer.parseInt(num1) + Integer.parseInt(num2)
+            binding.tvResult2.setText(result.toString());
+        }
     }
 }
