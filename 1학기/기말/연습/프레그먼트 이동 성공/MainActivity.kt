11c1c3d3f6fe191c6f1c9f5eee9cdc1f48e14d27
Lee Eunjun <85077262+moolgutree@users.noreply.github.com>
2024-06-04 00:38:53 +0900

    Create MainActivity.kt

---
+++ 1학기/기말/연습/프레그먼트 이동 성공/MainActivity.kt
@@ -0,0 +1,30 @@
+package kr.ac.dmu.pratice060301
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ReportFragment.Companion.reportFragment
+import kr.ac.dmu.pratice060301.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            replaceFragment(MemoFragment())
+        }
+        binding.button3.setOnClickListener {
+            replaceFragment(FilenameFragment())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainerView, fragment)
+        fragmentTransaction.commit()
+    }
+}
