7fbd07786798b06f672d13184bdf9971a444b47d
Lee Eunjun <85077262+moolgutree@users.noreply.github.com>
2024-06-13 16:38:42 +0900

    Create FilenameActivity.kt

---
+++ 1학기/기말/시험/FilenameActivity.kt
@@ -0,0 +1,22 @@
+package kr.ac.dmu.testteacher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.dmu.testteacher.databinding.ActivityFilenameBinding
+import kr.ac.dmu.testteacher.databinding.ActivityMainBinding
+
+class FilenameActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_filename)
+        var binding = ActivityFilenameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener() {
+            // SharedPreference에 이름 저장
+            Myapplication.preferences.setString("filename", "abc.txt")
+            finish()
+        }
+    }
+}
