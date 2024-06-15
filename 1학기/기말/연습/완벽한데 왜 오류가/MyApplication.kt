fc8a7883300d5b0fa90b66751adcfcf7253d242d
Lee Eunjun <85077262+moolgutree@users.noreply.github.com>
2024-06-15 17:08:41 +0900

    Create MyApplication.kt

---
+++ 1학기/기말/연습/완벽한데 왜 오류가/MyApplication.kt
@@ -0,0 +1,15 @@
+package kr.ac.dmu.pratice061501
+
+import android.app.Application
+
+class MyApplication: Application() {
+
+    companion object {
+        lateinit var preferences: PreferenceUtil   // 사용자 정의 타입(안드로이드 타입 아님)
+    }
+
+    override fun onCreate() {
+        preferences = PreferenceUtil(applicationContext) // 앱 자기 자신
+        super.onCreate()
+    }
+}
